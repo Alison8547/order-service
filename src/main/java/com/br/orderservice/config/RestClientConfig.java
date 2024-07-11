89745473241412087bf36ab7ab15de746587b145
Alison8547 <alisonailson100@hotmail.com>
2024-07-11 18:22:34 -0300

    circuit breaker

--- src/main/java/com/br/orderservice/config/RestClientConfig.java
+++ src/main/java/com/br/orderservice/config/RestClientConfig.java
@@ -2,11 +2,16 @@ package com.br.orderservice.config;
 
 import com.br.orderservice.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -17,6 +22,7 @@ public class RestClientConfig {
     @Bean
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
+                .requestFactory(getClientRequestFactory())
                 .baseUrl(urlInventory)
                 .build();
 
@@ -25,5 +31,12 @@ public class RestClientConfig {
         var httpServiceProxy = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
 
         return httpServiceProxy.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
     }
 }
